086f5c23191ca434dd562484ff3ee6a2b3b021d7
Shruthi Nagaraju <98145846+mnkshruthi@users.noreply.github.com>
2023-05-22 18:28:10 -0400

    Usestate Hook

--- src/components/Ignore/AUseState.js
+++ src/components/Ignore/AUseState.js
@@ -9,6 +9,7 @@ export const AUseState = () => {
   return (
     <>
       <div>
+        <h1>UseState Hook</h1>
         <button onClick={() => setresourceType("posts")}>Posts</button>
         <button onClick={() => setresourceType("Users")}>Users</button>
         <button onClick={() => setresourceType("Comments")}>Comments</button>
